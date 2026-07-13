a696e5c7edb1f0ce9ef8e0963ff42e68bffa9c75
JacksonVago <jackson@natividadesolucoes.com.br>
2026-07-13 15:56:41 -0300

    ssl_4

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: 'http://localhost:98'
-  baseURL: 'https://www.adminimove.com.br:98'
+  //baseURL: 'https://www.adminimove.com.br:98'
+  baseURL: 'https://20.51.104.223:98'
 });
 
 export default api
