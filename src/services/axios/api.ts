4336892ef6f7a8c7ed6ec0026b7ae698e38e5801
JacksonVago <jackson@natividadesolucoes.com.br>
2026-07-13 15:49:39 -0300

    ssl_2

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: 'http://localhost:98'
-  baseURL: 'http://20.51.104.223:98'
+  baseURL: 'https://20.51.104.223:98'
 });
 
 export default api
